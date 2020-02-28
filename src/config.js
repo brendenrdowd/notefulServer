747a776a1181dbf96da551e0008563705dd745f9
Fernando Gaol <ffilho@riolindo.org>
2020-02-27 21:29:53 -0700

    database deployed

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'production',
   API_TOKEN: process.env.API_TOKEN || 'dummy key',
-  DB_URL: process.env.DB_URL || 'postgresql'
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql'
 };
