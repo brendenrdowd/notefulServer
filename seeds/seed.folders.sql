2577f2b27eaca83dbab63e84caa999be89e75b35
Fernando Gaol <ffilho@riolindo.org>
2020-02-27 20:34:15 -0700

    third commit

--- seeds/seed.folders.sql
+++ seeds/seed.folders.sql
@@ -1,4 +1,4 @@
-INSERT INTO folders(name,id)
+INSERT INTO folders(id, name)
 VALUES ("b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1","Important"),
 ("b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1","Super"),
 ("b07162f0-ffaf-11e8-8eb2-f2801f1b9fd1","Spangley");
--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const express = require('express'),
   app = express(),
   errorHandler = require('./errorHandler'),
   NODE_ENV = require('./config'),
-  notefulRouter = require('./noteful-routers/noteful-router')
+  notefulRouter = require('./noteful-routers/noteful-router');
 
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
@@ -14,22 +14,10 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-
 app.use('/api/noteful', notefulRouter);
 
 app.get('/api/noteful', (req, res) => {
   res.send('Welcome to the Noteful API!');
-});
-
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
 });
 
 app.use(errorHandler);
