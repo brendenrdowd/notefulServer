ffcb8706270c72a08919157a60894c6716580545
Fernando Gaol <ffilho@riolindo.org>
2020-02-27 19:56:48 -0700

    second commit

--- src/app.js
+++ src/app.js
@@ -1,31 +1,37 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express'),
   morgan = require('morgan'),
   cors = require('cors'),
   helmet = require('helmet'),
-  app = express();
+  app = express(),
+  errorHandler = require('./errorHandler'),
+  NODE_ENV = require('./config'),
+  notefulRouter = require('./noteful-routers/noteful-router')
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!')
-})
+
+app.use('/api/noteful', notefulRouter);
+
+app.get('/api/noteful', (req, res) => {
+  res.send('Welcome to the Noteful API!');
+});
 
 app.use(function errorHandler(error, req, res, next) {
-  let response
+  let response;
   if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } }
+    response = { error: { message: 'server error' } };
   } else {
-    console.error(error)
-    response = { message: error.message, error }
+    console.error(error);
+    response = { message: error.message, error };
   }
-  res.status(500).json(response)
-})
+  res.status(500).json(response);
+});
 
-module.exports = app
+app.use(errorHandler);
+
+module.exports = app;
